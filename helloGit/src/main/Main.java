aa6d879b3a1eb1fe51e675aa4e59fe6be95e1c7d
exodusFalse <kotonari64@gmail.com>
2020-07-29 10:20:12 +0900

    追加したプロジェクト

--- helloGit/src/main/Main.java
+++ helloGit/src/main/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Ghost in the shell");
+		System.out.println("Stand Alone Complex");
+		System.out.println("2nd G.I.G");
 	}
 
 }
